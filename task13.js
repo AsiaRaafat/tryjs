7a3dc915cf1da44e0e3ab49aa7eab7cbafe7e6a1
AsiaRaafat <asiaraafat52@gmail.com>
2024-08-19 22:02:53 +0300

    lasttask

--- task13.js
+++ task13.js
@@ -24,3 +24,33 @@ function multiplication(){
   let  multiply =num1*num2;
   console.log(`the sum of two numbers is ${multiply}`);
 }
+
+
+
+// microwave 
+let micro = false;
+ 
+function microwaveon (){
+  if (micro==false){
+    micro==true;
+    console.log("microwave is ON");
+  }
+}
+
+function timeon(){
+
+    setTimeout(foodisready,2000);
+}
+
+function microwaveoff(){
+  if (micro==true){
+    micro==false;
+    alert("microwave is OFF");
+  }else {
+    alert("microwave is OFF");
+  }
+
+}
+function foodisready(){
+    console.log("your food is ready");
+}
